5fe61d9b4932ad87f05d03dbd4d6d487ad97d4d5
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-10-11 09:15:01 +0200

    product category models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+bouquet = Category.new(
+  name: "Bouquets",
+  position: 10,
+  display: true
+)
+bouquet.save!
+print "1 category / "
+
+bouquet_printemps = Product.new(
+  name: "Bouquet Printemps",
+  description: "Un somptueux bouquet de fleurs composé par Solstice avec des fleurs de saison sur le thème du printemps. Il s’agit d’une idée cadeau idéale pour un anniversaire ou pour remercier vos proches de leur soutien. Vous pouvez entièrement personnaliser ce bouquet en fonction de vos préférences et de votre budget.",
+  min_price: 35,
+  position: 10,
+  available: true,
+  category: bouquet
+)
+# file = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694609863/Solstice/Bouquet_Printemps_apdtox.jpg")
+# file2 = URI.open("https://res.cloudinary.com/dqfgqm3cn/image/upload/v1694699688/Solstice/Bouquet_Printemps2_br3b9g.jpg")
+# bouquet_printemps.photos.attach(io: file, filename: "image.png", content_type: "image/png")
+# bouquet_printemps.photos.attach(io: file2, filename: "image.png", content_type: "image/png")
+
+bouquet_printemps.save!
+print "1 product / "
