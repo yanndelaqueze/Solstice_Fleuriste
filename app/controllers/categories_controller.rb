5bc1e8ac4501e8e27d521232ede0b955205949de
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-10-11 08:53:05 +0200

    products categories controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,52 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit update destroy]
+
+  def index
+    @categories = Category.all.order(position: :asc)
+    @category = Category.new
+  end
+
+  def show
+    @products = @category.products.order(position: :asc)
+    @product = Product.new
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Category was successfully updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, status: :see_other
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :description, :display, :position)
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,54 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
+  before_action :set_product, only: %i[show edit update destroy]
+
+  def index
+    @products = Product.all.order(order: :asc)
+  end
+
+  def show
+    @order_item = OrderItem.new
+  end
+
+  def new
+    @categories = Category.all
+    @product = Product.new
+  end
+
+  def create
+    @categories = Category.all
+    @product = Product.new(product_params)
+    @product.category_id = params[:product][:category_id]
+    if @product.save
+      redirect_to category_path(@product.category), notice: 'Product was successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to category_path(@product.category), notice: 'Product was successfully updated'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to request.referrer || root_url
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :min_price, :available, :position, :category_id)
+  end
 end
